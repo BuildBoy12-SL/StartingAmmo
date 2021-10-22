310580aa40c1a8272aa7bbcd2693456ccd675fdc
Build <jwbradley2003@gmail.com>
2021-10-22 15:10:17 -0400

    update defaults

--- StartingAmmo/Config.cs
+++ StartingAmmo/Config.cs
@@ -19,9 +19,73 @@ namespace StartingAmmo
         /// <summary>
         /// Gets or sets a collection of roles to their starting ammo.
         /// </summary>
-        public Dictionary<RoleType, Dictionary<ItemType, ushort>> Ammo { get; set; } = new Dictionary<RoleType, Dictionary<ItemType, ushort>>()
+        public Dictionary<RoleType, Dictionary<ItemType, ushort>> Ammo { get; set; } = new Dictionary<RoleType, Dictionary<ItemType, ushort>>
         {
             [RoleType.FacilityGuard] = new Dictionary<ItemType, ushort>
+            {
+                [ItemType.Ammo9x19] = 9999,
+                [ItemType.Ammo12gauge] = 9999,
+                [ItemType.Ammo44cal] = 9999,
+                [ItemType.Ammo556x45] = 9999,
+                [ItemType.Ammo762x39] = 9999,
+            },
+            [RoleType.NtfCaptain] = new Dictionary<ItemType, ushort>
+            {
+                [ItemType.Ammo9x19] = 9999,
+                [ItemType.Ammo12gauge] = 9999,
+                [ItemType.Ammo44cal] = 9999,
+                [ItemType.Ammo556x45] = 9999,
+                [ItemType.Ammo762x39] = 9999,
+            },
+            [RoleType.NtfSergeant] = new Dictionary<ItemType, ushort>
+            {
+                [ItemType.Ammo9x19] = 9999,
+                [ItemType.Ammo12gauge] = 9999,
+                [ItemType.Ammo44cal] = 9999,
+                [ItemType.Ammo556x45] = 9999,
+                [ItemType.Ammo762x39] = 9999,
+            },
+            [RoleType.NtfSpecialist] = new Dictionary<ItemType, ushort>
+            {
+                [ItemType.Ammo9x19] = 9999,
+                [ItemType.Ammo12gauge] = 9999,
+                [ItemType.Ammo44cal] = 9999,
+                [ItemType.Ammo556x45] = 9999,
+                [ItemType.Ammo762x39] = 9999,
+            },
+            [RoleType.NtfPrivate] = new Dictionary<ItemType, ushort>
+            {
+                [ItemType.Ammo9x19] = 9999,
+                [ItemType.Ammo12gauge] = 9999,
+                [ItemType.Ammo44cal] = 9999,
+                [ItemType.Ammo556x45] = 9999,
+                [ItemType.Ammo762x39] = 9999,
+            },
+            [RoleType.ChaosConscript] = new Dictionary<ItemType, ushort>
+            {
+                [ItemType.Ammo9x19] = 9999,
+                [ItemType.Ammo12gauge] = 9999,
+                [ItemType.Ammo44cal] = 9999,
+                [ItemType.Ammo556x45] = 9999,
+                [ItemType.Ammo762x39] = 9999,
+            },
+            [RoleType.ChaosMarauder] = new Dictionary<ItemType, ushort>
+            {
+                [ItemType.Ammo9x19] = 9999,
+                [ItemType.Ammo12gauge] = 9999,
+                [ItemType.Ammo44cal] = 9999,
+                [ItemType.Ammo556x45] = 9999,
+                [ItemType.Ammo762x39] = 9999,
+            },
+            [RoleType.ChaosRepressor] = new Dictionary<ItemType, ushort>
+            {
+                [ItemType.Ammo9x19] = 9999,
+                [ItemType.Ammo12gauge] = 9999,
+                [ItemType.Ammo44cal] = 9999,
+                [ItemType.Ammo556x45] = 9999,
+                [ItemType.Ammo762x39] = 9999,
+            },
+            [RoleType.ChaosRifleman] = new Dictionary<ItemType, ushort>
             {
                 [ItemType.Ammo9x19] = 9999,
                 [ItemType.Ammo12gauge] = 9999,
